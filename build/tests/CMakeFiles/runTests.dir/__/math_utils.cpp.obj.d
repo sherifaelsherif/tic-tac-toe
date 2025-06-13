fad523dc7512e69b8e59248ffbdb98a5cb71da35
sherifaelsherif <sherifaelsherif18@gmail.com>
2025-06-13 21:13:39 +0300

    ready

---
+++ build/tests/CMakeFiles/runTests.dir/__/math_utils.cpp.obj.d
@@ -0,0 +1,3 @@
+tests/CMakeFiles/runTests.dir/__/math_utils.cpp.obj: \
+ C:\Users\usar\Downloads\mygithub\tic-tac-toe\math_utils.cpp \
+ C:\Users\usar\Downloads\mygithub\tic-tac-toe\math_utils.h
---
+++ main.cpp
@@ -0,0 +1,7 @@
+ #include <iostream>
+#include "math_utils.h"  // declares add()
+
+int main() {
+    std::cout << "2 + 3 = " << add(2, 3) << std::endl;
+    return 0;
+}
